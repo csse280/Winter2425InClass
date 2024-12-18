3f6c186de40b787a2ffdfe10fda4d2e6b6bfabcf
David Fisher <fisherds@rose-hulman.edu>
2024-12-18 11:39:01 -0500

    html and css done on the bottom

--- Homeworks/HW1/public/scripts/main.js
+++ Homeworks/HW1/public/scripts/main.js
@@ -8,8 +8,26 @@ rhit.counter = 0;
 
 rhit.setupButtons = function () {
 	console.log("Setup the buttons");
+	document.querySelector("#decButton").onclick = () => {
+		console.log("The pressed Decrement");
+		rhit.counter -= 1;
+		rhit.update();
+	};
+	document.querySelector("#resetButton").onclick = () => {
+		console.log("The pressed Reset");
+		rhit.counter = 0;
+		rhit.update();
+	};
+	document.querySelector("#incButton").onclick = () => {
+		console.log("The pressed Increment");
+		rhit.counter += 1;
+		rhit.update();
+	};
 };
 
+rhit.update = function() {
+	document.querySelector("#counterText").innerHTML = rhit.counter;
+};
 
 rhit.main = function () {
 	console.log("Ready");
