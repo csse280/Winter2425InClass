6c15464430e97067af911e41fea7c310fc4a7466
David Fisher <fisherds@rose-hulman.edu>
2024-12-19 10:00:39 -0500

    HW1 solution

--- Homeworks/HW1/public/scripts/main.js
+++ Homeworks/HW1/public/scripts/main.js
@@ -5,6 +5,10 @@
 
 var rhit = rhit || {};
 rhit.counter = 0;
+rhit.colorText = "#800000";
+rhit.colorClass = "red-box";
+// rhit.colorText = "Hello Class";
+// rhit.colorClass = "purple-box";
 
 rhit.setupButtons = function () {
 	console.log("Setup the buttons");
@@ -23,15 +27,28 @@ rhit.setupButtons = function () {
 		rhit.counter += 1;
 		rhit.update();
 	};
+	let allColorButtons = document.querySelectorAll(".color-button");
+	for (let colorButton of allColorButtons) {
+		colorButton.onclick = (event) => {
+			console.log("You pressed some color", event.target);
+			rhit.colorText = event.target.dataset.colorText;
+			rhit.colorClass = event.target.dataset.colorClass;
+			rhit.update();
+		}
+	}
 };
 
 rhit.update = function() {
 	document.querySelector("#counterText").innerHTML = rhit.counter;
+
+	document.querySelector("#favoriteColorBox").innerHTML = rhit.colorText;
+	document.querySelector("#favoriteColorBox").classList = [rhit.colorClass];
 };
 
 rhit.main = function () {
 	console.log("Ready");
 	rhit.setupButtons();
+	rhit.update();
 };
 
 rhit.main();
