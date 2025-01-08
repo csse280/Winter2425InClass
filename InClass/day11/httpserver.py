48c74818c78e808356792a0b4dca571f3f48ac06
David Fisher <fisherds@rose-hulman.edu>
2025-01-08 11:00:08 -0500

    my typed code

--- InClass/day11/httpserver.py
+++ InClass/day11/httpserver.py
@@ -10,8 +10,8 @@ import traceback
 # Set a constant to determine where we should be loaded files from
 # It is a good idea to put your public files here and not your private files
 # Why might that be a good idea?
-#TODO change to "./public"
-WEB_HOME = "."
+# Done change to "./public"
+WEB_HOME = "./public"
 
 
 #   1. Practice Debugging via the Python Debugger
@@ -24,6 +24,72 @@ WEB_HOME = "."
 #   8. Make it so that all existing items (see data in script.js at top) get added when the page loads
 #   As time allows:
 #   9. Let's look at an example of dynamic rendering a page instead of javascript
+
+def get_headers(reader_from_browser):
+    headers = {}
+    header_line = reader_from_browser.readline().decode("utf-8")
+    while(True):
+        if(header_line == '\r\n'):
+            break
+        pair = header_line.split(": ")
+        headers[pair[0]] = pair[1]
+        header_line = reader_from_browser.readline().decode("utf-8")
+    return headers
+
+def get_post_data(reader_from_browser, headers):
+    request_body = reader_from_browser.read(int(headers["Content-Length"]))
+    post_lines = request_body.decode("utf-8").split("\r\n")
+
+    post_data = {}
+    if(headers["Content-Type"] == "text/plain"):
+        fields = post_lines.split("\r\n")
+        for field in fields:
+            if(field == ""):
+                continue
+            split_field = field.split("=")
+            post_data[split_field[0]] = split_field[1]
+    else:
+        fields = request_body.split("&")
+        for field in fields:
+            if(field == ""):
+                continue
+            split_field = field.split("=")
+            post_data[urllib.parse.unquote_plus(split_field[0])] = urllib.parse.unquote_plus(split_field[1])
+    return post_data
+
+
+def get_content_type(file_extension):
+    if(file_extension == "html"):
+        content_type = 'text/html; charset=utf-8'
+    elif(file_extension == "png"):
+        content_type = 'image/png'
+    elif(file_extension == "ico"):
+        #https://www.w3schools.com/html/html_favicon.asp
+        content_type = 'image/x-icon'
+    elif(file_extension == "jpeg"):
+        content_type = 'image/jpeg'
+    elif(file_extension == "js"):
+        content_type = 'text/javascript; charset=utf-8'
+    elif(file_extension == "css"):
+        content_type = 'text/css; charset=utf-8'
+    else:
+        #let's try to allow reading anything as html
+        content_type = 'text/html; charset=utf-8'
+    return content_type
+
+
+def write_to_browser(writer_to_browser, response_body, content_type):
+    response_headers = bytearray("\r\n".join([
+        'HTTP/1.1 200 OK',
+        f'Content-Type: {content_type}',
+        f'Content-length: {len(response_body)}',
+        'Connection: close',
+        '\r\n'
+    ]), encoding = "utf-8")
+
+    writer_to_browser.write(response_headers)
+    writer_to_browser.write(response_body)
+    writer_to_browser.flush()
 
 
 def main():
@@ -44,38 +110,14 @@ def main():
                     request_line = request_line.decode("utf-8")
                     request_method = request_line.split(' ')[0]
                     file_name = request_line.split(' ')[1]
-                    file_extension = file_name.split(".")[2]
+                    file_extension = file_name.split(".")[1]
 
-                    headers = {}
-                    header_line = reader_from_browser.readline().decode("utf-8")
-                    while(True):
-                        if(header_line == '\r\n'):
-                            break
-                        pair = header_line.split(": ")
-                        headers[pair[0]] = pair[1]
-                        header_line = reader_from_browser.readline().decode("utf-8")
+                    headers = get_headers(reader_from_browser)
                     
                     #in case we want to check it later
                     post_data="" 
                     if(request_method == "POST"):
-                        request_body = reader_from_browser.read(int(headers["Content-Length"]))
-                        post_lines = request_body.decode("utf-8").split("\r\n")
-
-                        post_data = {}
-                        if(headers["Content-Type"] == "text/plain"):
-                            fields = post_lines.split("\r\n")
-                            for field in fields:
-                                if(field == ""):
-                                    continue
-                                split_field = field.split("=")
-                                post_data[split_field[0]] = split_field[1]
-                        else:
-                            fields = request_body.split("&")
-                            for field in fields:
-                                if(field == ""):
-                                    continue
-                                split_field = field.split("=")
-                                post_data[urllib.parse.unquote_plus(split_field[0])] = urllib.parse.unquote_plus(split_field[1])                        
+                        post_data = get_post_data(reader_from_browser, headers)                        
 
                 except socket.timeout:
                     continue
@@ -83,7 +125,7 @@ def main():
                     exit(0)
                 except Exception as e:
                     print("Error after reader_from_browser")
-                    print(e)
+                    print(traceback.format_exc())
 
             if(file_name == "/shutdown"):
                 print("Shutting down")
@@ -95,47 +137,14 @@ def main():
                     file_name = f"{WEB_HOME}{file_name}"
                     with(open(file_name, "rb") as fd):
                         response_body = bytearray(fd.read())
+                        content_type = get_content_type(file_extension)
 
-                        if(file_extension == "html"):
-                            content_type = 'text/html; charset=utf-8'
-                        elif(file_extension == "png"):
-                            content_type = 'image/png'
-                        elif(file_extension == "ico"):
-                            #https://www.w3schools.com/html/html_favicon.asp
-                            content_type = 'image/x-icon'
-                        elif(file_extension == "jpeg"):
-                            content_type = 'image/jpeg'
-                        elif(file_extension == "js"):
-                            content_type = 'text/javascript; charset=utf-8'
-                        elif(file_extension == "css"):
-                            content_type = 'text/css; charset=utf-8'
-                        else:
-                            #let's try to allow reading anything as html
-                            content_type = 'text/html; charset=utf-8'
-
-                    response_headers = bytearray("\r\n".join([
-                        'HTTP/1.1 200 OK',
-                        f'Content-Type: {content_type}',
-                        f'Content-length: {len(response_body)}',
-                        'Connection: close',
-                        '\r\n'
-                    ]), encoding = "utf-8")
-
-                    print()
-                    print('Response headers:')
-                    print(response_headers)
-                    print()
-                    print('Response body:')
-                    print(response_body)
-                    print()
-
-                    writer_to_browser.write(response_headers)
-                    writer_to_browser.write(response_body)
-                    writer_to_browser.flush()
+                    write_to_browser(writer_to_browser, response_body, content_type)
+                    
                 except Exception as e:
-                    # TODO: handle error in a better manner
+                    # Done: handle error in a better manner
                     print("Error after writer_to_browser")
-                    print(e)
+                    print(traceback.format_exc())
                     #TODO - 1. get an error traceback as a string
                     #TODO - 2. use a special error page to show results
                     #TODO - 3. insert error trace into page
