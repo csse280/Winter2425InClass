92381aef8f1115e61583bd979a77fbbe9e139980
David Fisher <fisherds@rose-hulman.edu>
2025-01-14 11:52:13 -0500

    HW progress

--- Homeworks/HW2/public/scripts/main.js
+++ Homeworks/HW2/public/scripts/main.js
@@ -1,34 +1,29 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
 /** globals */
-rhit.variableName = "";
+rhit.receiveBoard = "0110110";
+rhit.receiveNumMoves = 5;
+rhit.receiveIsWon = false;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
+
+rhit.sendNewGameRequest = function () {
+
 };
 
-rhit.ClassName = class {
-	constructor() {
+rhit.sendButtonPressRequest = function (pressIndex) {
 
-	}
+};
 
-	methodName() {
+rhit.updateView = function() {
+	
+};
 
-	}
-}
+rhit.setupButtons = function () {
+
+};
+
 
 /* Main */
-/** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
 };
