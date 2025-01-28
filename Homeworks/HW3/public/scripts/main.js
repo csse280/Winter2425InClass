c74487eccce072516d1d625f696ef29a0bbabbea
David Fisher <fisherds@rose-hulman.edu>
2025-01-28 11:15:04 -0500

    progress on HTML CSS

--- Homeworks/HW3/public/scripts/main.js
+++ Homeworks/HW3/public/scripts/main.js
@@ -3,3 +3,20 @@
 // Temporary learning stuff...
 
 console.log("Ready");
+
+document.querySelector("#fakeWin").addEventListener("click", () => {
+
+	// TODO: Actually determine if they made the leadderboard!
+	
+  console.log("TODO: Show the dialog!");
+  $("#addNameDialog").modal("show");
+});
+
+document.querySelector("#addNameBtn").addEventListener("click", () => {
+
+	// TODO: Actually send the name to the server!
+
+	document.querySelector("#addNameBtn").blur();
+	$("#addNameDialog").modal("hide");
+
+});
