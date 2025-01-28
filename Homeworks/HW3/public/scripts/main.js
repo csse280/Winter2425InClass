86749c9904ed1d3e7017118a5e37c15dbfc9e9ea
David Fisher <fisherds@rose-hulman.edu>
2025-01-28 11:21:25 -0500

    Hardcoded HTML and CSS for the Dialog and Leaderboard

--- Homeworks/HW3/public/scripts/main.js
+++ Homeworks/HW3/public/scripts/main.js
@@ -7,16 +7,17 @@ console.log("Ready");
 document.querySelector("#fakeWin").addEventListener("click", () => {
 
 	// TODO: Actually determine if they made the leadderboard!
-	
+
   console.log("TODO: Show the dialog!");
   $("#addNameDialog").modal("show");
 });
 
 document.querySelector("#addNameBtn").addEventListener("click", () => {
+	document.querySelector("#addNameBtn").blur();
+	let name = document.querySelector("#nameInput").value;
+	console.log("Name: ", name);
 
 	// TODO: Actually send the name to the server!
 
-	document.querySelector("#addNameBtn").blur();
 	$("#addNameDialog").modal("hide");
-
 });
