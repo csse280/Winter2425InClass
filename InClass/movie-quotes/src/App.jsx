ed6be15f31d32ce21002693f9ce267f12c10d2be
David Fisher <fisherds@rose-hulman.edu>
2025-02-06 11:18:27 -0500

    comment out the spike

--- InClass/movie-quotes/src/App.jsx
+++ InClass/movie-quotes/src/App.jsx
@@ -1,45 +1,45 @@
-import { collection, addDoc, serverTimestamp } from "firebase/firestore"; 
-import { query, where, onSnapshot } from "firebase/firestore";
-import { db } from "./firebaseConfig.js";
+// import { collection, addDoc, serverTimestamp } from "firebase/firestore"; 
+// import { query, where, onSnapshot } from "firebase/firestore";
+// import { db } from "./firebaseConfig.js";
 import "./App.css";
 
 
 function App() {
   
-  function spikeTestRead() {
-    console.log("TODO: Read from the firestore database");
-    // const q = query(collection(db, "MovieQuotes"), where("state", "==", "CA"));
-    const ref = collection(db, "MovieQuotes");
-    const unsubscribe = onSnapshot(ref, (querySnapshot) => {
+  // function spikeTestRead() {
+  //   console.log("TODO: Read from the firestore database");
+  //   // const q = query(collection(db, "MovieQuotes"), where("state", "==", "CA"));
+  //   const ref = collection(db, "MovieQuotes");
+  //   const unsubscribe = onSnapshot(ref, (querySnapshot) => {
       
-      querySnapshot.forEach((doc) => {  
-        console.log("Document id:", doc.id);
-        console.log("Document data:", doc.data());
-      });
+  //     querySnapshot.forEach((doc) => {  
+  //       console.log("Document id:", doc.id);
+  //       console.log("Document data:", doc.data());
+  //     });
       
-    });
-  }
+  //   });
+  // }
 
-  async function spikeTestWrite() {
-    console.log("Write to the firestore database");
-    try {
-      const docRef = await addDoc(collection(db, "MovieQuotes"), {
-        quote: "Spike quote",
-        movie: "Spike movie",
-        lastTouched: serverTimestamp(),
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  }
+  // async function spikeTestWrite() {
+  //   console.log("Write to the firestore database");
+  //   try {
+  //     const docRef = await addDoc(collection(db, "MovieQuotes"), {
+  //       quote: "Spike quote",
+  //       movie: "Spike movie",
+  //       lastTouched: serverTimestamp(),
+  //     });
+  //     console.log("Document written with ID: ", docRef.id);
+  //   } catch (e) {
+  //     console.error("Error adding document: ", e);
+  //   }
+  // }
 
   return (
     <>
-      <button onClick={spikeTestRead}>Spike Read Test</button>
+      {/* <button onClick={spikeTestRead}>Spike Read Test</button>
       <br />
       <br />
-      <button onClick={spikeTestWrite}>Spike Write Test</button>
+      <button onClick={spikeTestWrite}>Spike Write Test</button> */}
     </>
   );
 }
