3bba36de7b474d09bbcd8620fdd5bd24944beeec
David Fisher <fisherds@rose-hulman.edu>
2025-02-13 11:31:33 -0500

    App Bar Detail page is done

---
+++ InClass/movie-quotes/src/DetailAppBar.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+export default function DetailAppBar({onEdit, onDelete}) {
+  return (
+    <AppBar position="fixed"  sx={{ backgroundColor: 'var(--color-rosered)' }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Movie Quotes
+        </Typography>
+        <IconButton
+          color="inherit"
+          aria-label="edit"
+          onClick={onEdit}
+        >
+          <EditIcon />
+        </IconButton>
+        <IconButton
+          color="inherit"
+          aria-label="delete"
+          onClick={onDelete}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
--- InClass/movie-quotes/src/MovieQuoteDetailPage.jsx
+++ InClass/movie-quotes/src/MovieQuoteDetailPage.jsx
@@ -1,13 +1,22 @@
 import { useParams } from "react-router-dom";
+import DetailAppBar from "./DetailAppBar";
 
 export default function MovieQuoteDetailPage() {
-    const { id } = useParams();
+  const { id } = useParams();
 
+  return (
+    <DetailAppBar
+      onEdit={() => {
+        console.log("Clicked edit");
+      }}
+      onDelete={() => {
+        console.log("Clicked delete");
+      }}
+    />
 
-    return (
-        <div>
-          <h2>Movie Quote Detail</h2>
-          <p>Quote ID: {id}</p>
-        </div>
-    );
+    // <div>
+    //   <h2>Movie Quote Detail</h2>
+    //   <p>Quote ID: {id}</p>
+    // </div>
+  );
 }
