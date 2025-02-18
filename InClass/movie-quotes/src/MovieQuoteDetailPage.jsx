ef5273d42ecdaf9f79bf0dfc98a114e16e8e0680
David Fisher <fisherds@rose-hulman.edu>
2025-02-18 10:38:31 -0500

    look of the deatil page

--- InClass/movie-quotes/src/MovieQuoteDetailPage.jsx
+++ InClass/movie-quotes/src/MovieQuoteDetailPage.jsx
@@ -1,22 +1,42 @@
 import { useParams } from "react-router-dom";
 import DetailAppBar from "./DetailAppBar";
+import { useNavigate } from "react-router-dom";
+import { Card, CardContent, Typography } from "@mui/material";
+import { FormatQuote, Movie } from "@mui/icons-material";
 
 export default function MovieQuoteDetailPage() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   return (
-    <DetailAppBar
-      onEdit={() => {
-        console.log("Clicked edit");
-      }}
-      onDelete={() => {
-        console.log("Clicked delete");
-      }}
-    />
-
-    // <div>
-    //   <h2>Movie Quote Detail</h2>
-    //   <p>Quote ID: {id}</p>
-    // </div>
+    <>
+      <DetailAppBar
+        onHome={() => {
+          console.log("Clicked Home");
+          navigate("/");
+        }}
+        onEdit={() => {
+          console.log("Clicked edit");
+        }}
+        onDelete={() => {
+          console.log("Clicked delete");
+        }}
+      />
+      <div className="p-20 bg-gray-300 min-h-screen">
+        <Typography variant="h5">Quote:</Typography>
+        <Card>
+          <CardContent>
+            <FormatQuote /> {"My quote will go here"}
+          </CardContent>
+        </Card>
+        <br></br>
+        <Typography variant="h5">Movie:</Typography>
+        <Card>
+          <CardContent>
+            <Movie /> {"My movie title will go here"}
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 }
